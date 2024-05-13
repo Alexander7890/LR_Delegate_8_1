a3a11c8a55c8cf733e9e71c52f7bc92895e717b1
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-05-13 18:45:51 +0300

    Add project files.

---
+++ LR_Delegate_8_1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LR_Delegate_8_1
+{
+    public partial class Form1 : Form
+    {
+        Person person;// Посилання на об'єкт класу Person
+        public Form1()
+        {
+            InitializeComponent();
+            person = new Person("Петров", "студент");
+            // Додавання елементів у ComboBox при ініціалізації форми
+            comboBox1.Items.AddRange(new object[] { "студент", "школяр", "робітник" });
+            comboBox1.SelectedIndex = 0; // Встановлення початкового вибору
+        }
+        // Виклик методу RestMethod
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(person.RestMethod());
+        }
+        // Зміна статусу
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string newStatus = comboBox1.SelectedItem.ToString();
+            person.ChangeStatus(newStatus);
+        }
+    }
+}
---
+++ LR_Delegate_8_1/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_Delegate_8_1
+{
+    public delegate string RestDelegate();
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public RestDelegate RestMethod { get; set; }
+        // Конструктор
+        public Person(string name, string status)
+        {
+            Name = name;
+            Status = status;
+            switch (status)
+            {
+                case "студент":
+                    RestMethod = Studying;
+                    break;
+                case "школяр":
+                case "робітник":
+                    RestMethod = Resting;
+                    break;
+            }
+        }
+        // Методи
+        public void ChangeStatus(string newStatus)// Зміна статусу
+        {
+            Status = newStatus;
+            switch (newStatus)
+            {
+                case "студент":
+                    RestMethod = Studying;
+                    break;
+                case "школяр":
+                case "робітник":
+                    RestMethod = Resting;
+                    break;
+            }
+        }
+        // Методи, що викликаються делегатом
+        private string Resting()
+        {
+            return $"{Name} відпочиває";
+        }
+        // Методи, що викликаються делегатом
+        private string Studying()
+        {
+            return $"{Name} вчиться";
+        }
+    }
+}
